5b734d5161060c1834d10e5f2a40612c77063392
fuad fajri soba <fuadfajrisoba@gmail.com>
2024-04-04 10:38:38 +0700

    fetch news api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import Navbar from './components/Navbar';
+import Container from './components/Container';
+import Loading from './components/Loading';
+import Error from './components/Error';
+
+import { getNews } from './services/getNews';
+import { useEffect } from 'react';
+
 function App() {
+  
+  useEffect(() => {
+    const fetchTechNews = async () => {
+      const res = await getNews({
+        searchQuery: 'microsoft'
+      });
+      console.log(res)
+    }
+
+    fetchTechNews();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <h1>Hello World!</h1>
+      </Container>
+    </>
   );
 }
 
