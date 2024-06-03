76b92bdd065b2f0530410cada6195642b85a4eb7
fuad fajri soba <fuadfajrisoba@gmail.com>
2024-06-03 11:09:54 +0700

    update navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,36 @@
 import styles from './Navbar.module.css';
 import newsIcon from '../../assets/news-icon.svg';
 
+import { useState } from 'react';
+import {Link} from "react-router-dom";
+import { CATEGORIES } from '../../constants/categories';
+import classnames from 'classnames';
+
 const Navbar = () => {
+    const [selected, setSelected] = useState('');
     return (
         <nav className={styles.nav}>
             <div className={styles.navIconWrapper}>
                 <img src={newsIcon} alt="navbar icon" className={styles.navIcon} />
                 <h1 className={styles.navTitle}>NEWS</h1>
+            </div>
+
+            <div className={styles.categories}>
+                {CATEGORIES.map((category, index) => {
+                    return (
+                        <Link
+                            key={index}
+                            onClick={() => setSelected(category.name)}
+                            to={`/${category.slug}`}
+                            className={classnames(styles.category, {
+                                [styles.selected] : selected === category.name
+                            })}
+                        >
+                            {category.name}
+                        </Link>
+                        // <div></div>
+                    )
+                })}
             </div>
         </nav>
     )
---
+++ src/constants/categories.js
@@ -0,0 +1,30 @@
+export const CATEGORIES = [
+    {
+        name: 'Apple',
+        slug: 'apple',
+    },
+    {
+        name: 'Playstation',
+        slug: 'playstation',
+    },
+    {
+        name: 'Microsoft',
+        slug: 'microsoft',
+    },
+    {
+        name: 'Google',
+        slug: 'google',
+    },
+    {
+        name: 'Netflix',
+        slug: 'netflix',
+    },
+    {
+        name: 'Games',
+        slug: 'games',
+    },
+    {
+        name: 'Nintendo',
+        slug: 'nintendo',
+    },
+]
